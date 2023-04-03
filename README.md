b098e6733e1d40df12a0af39a9b825d9196e07f6
enitial <spam@literecords.com>
2023-04-03 22:52:41 +0100

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Vite ReactJS + TailwindCSS
+
+This is a barebones vite react javascipt project with tailwindcss.
+
+### Install
+
+```
+npx tiged alexmnet/vite-react-tailwind <your-repo-name>
+```
+
+```
+cd <your-repo-name>
+```
+
+```
+npm install
+```
+
+```
+npm run dev
+```
+
+### Resources
+
+- [Vite](https://vitejs.dev/)
+- [React](https://react.dev/)
+- [TailwindCSS](https://tailwindcss.com/)
+- [Tiged](https://github.com/tiged/tiged)
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-gray-800 flex flex-col justify-center items-center text-center text-slate-300">
+      <h1 className="text-6xl mb-4 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500">
+        Vite - React - Tailwind
+      </h1>
+      <p className="text-xl capitalize">
+        github template from <strong>DecafDev</strong>
+      </p>
+    </div>
+  );
+}
